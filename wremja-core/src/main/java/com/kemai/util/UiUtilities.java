39dafce6e771a7124497aca8c8bb49eacd5d3027
Christoph Kutzinski <kutzi@gmx.de>
2009-05-10 15:40:03 +0000

    WREMJA-3: tried (!) to make the tray icon look nicer on Linux https://kenai.com/jira/browse/WREMJA-3

---
+++ wremja-core/src/main/java/com/kemai/util/UiUtilities.java
@@ -0,0 +1,157 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (martin_oat@yahoo.de)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
+
+package com.kemai.util;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import com.kemai.wremja.logging.Logger;
+
+/**
+ * Provides utilities for UI stuff.
+ * 
+ * @author Til Schneider, www.murfman.de
+ */
+/**
+ * @author MadMan
+ * 
+ */
+public class UiUtilities {
+
+    private static final Logger log = Logger.getLogger(UiUtilities.class);
+    
+    /**
+     * Scale Icons to a specific width. The aspect ratio is kept.
+     * 
+     * @param icon
+     *            The icon to scale.
+     * @param newWidth
+     *            The new width of the icon.
+     * @return The scaled Icon.
+     */
+    public static Icon scaleIcon(Icon icon, int newWidth) {
+        return scaleIcon(icon, newWidth, (int) ((newWidth / (float) icon
+                .getIconWidth()) * icon.getIconHeight()));
+    }
+
+    /**
+     * Scales Icons to a specific size
+     * 
+     * @param icon
+     *            Icon that should be scaled
+     * @param x
+     *            new X-Value
+     * @param y
+     *            new Y-Value
+     * @return Scaled Icon
+     */
+    public static Icon scaleIcon(Icon icon, int x, int y) {
+        int currentWidth = icon.getIconWidth();
+        int currentHeight = icon.getIconHeight();
+        if ((currentWidth == x) && (currentHeight == y)) {
+            return icon;
+        }
+        try {
+            // Create Image with Icon
+            BufferedImage iconimage = new BufferedImage(x, y,
+                    BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = iconimage.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                    RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+            g2.setRenderingHint(RenderingHints.KEY_RENDERING,
+                    RenderingHints.VALUE_RENDER_QUALITY);
+            AffineTransform z = g2.getTransform();
+            z.scale((double) x / currentWidth, (double) y / currentHeight);
+            g2.setTransform(z);
+            icon.paintIcon(null, g2, 0, 0);
+            g2.dispose();
+
+            // Return new Icon
+            return new ImageIcon(iconimage);
+
+        } catch (Exception e) {
+            log.error(e, e);
+        }
+
+        return icon;
+    }
+
+    /**
+     * Scales an image to a specific size and returns an BufferedImage
+     * 
+     * @param img
+     *            Scale this Image
+     * @param x
+     *            new X-Value
+     * @param y
+     *            new Y-Value
+     * @return Scaled BufferedImage
+     * 
+     * @since 2.5
+     */
+    public static BufferedImage scaleIconToBufferedImage(BufferedImage img,
+            int x, int y) {
+        return scaleIconToBufferedImage(img, x, y, img.getType());
+    }
+
+    /**
+     * Scales an image to a specific size and returns an BufferedImage
+     * 
+     * @param img
+     *            Scale this IMage
+     * @param x
+     *            new X-Value
+     * @param y
+     *            new Y-Value
+     * @param type
+     *            The type of the image.
+     * @return Scaled BufferedImage
+     * 
+     * @since 2.7
+     */
+    public static BufferedImage scaleIconToBufferedImage(Image img, int x,
+            int y, int type) {
+        // Scale Image
+        Image image = img.getScaledInstance(x, y, Image.SCALE_SMOOTH);
+
+        BufferedImage im = new BufferedImage(x, y, type);
+
+        Graphics2D g2 = im.createGraphics();
+        g2.drawImage(image, null, null);
+        g2.dispose();
+
+        im.flush();
+        return im;
+    }
+
+}
--- wremja-core/src/main/java/com/kemai/wremja/gui/GuiConstants.java
+++ wremja-core/src/main/java/com/kemai/wremja/gui/GuiConstants.java
@@ -1,18 +1,23 @@
 package com.kemai.wremja.gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
 
 import org.jdesktop.swingx.decorator.ColorHighlighter;
 import org.jdesktop.swingx.decorator.HighlightPredicate;
 import org.jdesktop.swingx.decorator.Highlighter;
 import org.jdesktop.swingx.decorator.HighlighterFactory;
+import org.jdesktop.swingx.util.OS;
 
+import com.kemai.util.UiUtilities;
 import com.kemai.wremja.logging.Logger;
 
 import edu.umd.cs.findbugs.annotations.SuppressWarnings;
@@ -34,10 +39,38 @@ public abstract class GuiConstants {
     public static final Color DARK_BLUE = new Color(64, 64, 128);
     
     /** The standard icon image. */
-    public static final Image NORMAL_ICON = new ImageIcon(GuiConstants.class.getResource("/icons/Baralga-Tray.gif")).getImage(); //$NON-NLS-1$
+    public static final Image NORMAL_ICON;
 
     /** The icon image when an activity is running. */
-    public static final Image ACTIVE_ICON = new ImageIcon(GuiConstants.class.getResource("/icons/Baralga-Tray-Green.png")).getImage(); //$NON-NLS-1$
+    public static final Image ACTIVE_ICON;
+    
+    
+    static {
+        BufferedImage normalIcon = null;
+        BufferedImage activeIcon = null;
+        if (SystemTray.isSupported()) {
+            try {
+                normalIcon = ImageIO.read(GuiConstants.class.getResource("/icons/Baralga-Tray.gif")); //$NON-NLS-1$
+                activeIcon = ImageIO.read(GuiConstants.class.getResource("/icons/Baralga-Tray-Green.png")); //$NON-NLS-1$
+                
+                if( OS.isWindows() ) {
+                    // no changes. For some reason the unscaled 32x32 icons look best on Windows
+                    // though the tray size is 16
+                } else {
+                    Dimension d = SystemTray.getSystemTray().getTrayIconSize();
+                
+                    normalIcon = UiUtilities.scaleIconToBufferedImage(normalIcon, d.width - 1 , d.height - 1,
+                        BufferedImage.TYPE_INT_ARGB);
+                    activeIcon = UiUtilities.scaleIconToBufferedImage(activeIcon, d.width - 1 , d.height - 1,
+                        BufferedImage.TYPE_INT_ARGB);
+                }
+            } catch (IOException e) {
+                log.error(e, e);
+            }
+        }
+        NORMAL_ICON = normalIcon;
+        ACTIVE_ICON = activeIcon;
+    }
 
     @SuppressWarnings(value="MS_MUTABLE_ARRAY", justification="We trust all callers")
     public static final Highlighter[] HIGHLIGHTERS = new Highlighter[] { 
