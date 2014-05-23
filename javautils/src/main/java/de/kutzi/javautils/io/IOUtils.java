72eb823c68de659086058e28fd148f985d47de4d
Christoph Kutzinski <ckutz@esailors.de>
2014-05-23 08:13:11 +0000

    Java 7 only

--- javautils/src/main/java/de/kutzi/javautils/io/IOUtils.java
+++ javautils/src/main/java/de/kutzi/javautils/io/IOUtils.java
@@ -2,17 +2,11 @@ package de.kutzi.javautils.io;
 
 import java.io.Closeable;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
-import org.jvnet.animal_sniffer.IgnoreJRERequirement;
-
-import de.kutzi.javautils.system.JavaUtil;
 
 public class IOUtils {
     private IOUtils() {
@@ -38,38 +32,6 @@ public class IOUtils {
      * are not supported. The behaviour seems to be platform dependent.
      */
     public static void copyFile(File source, File destination, boolean copyAttributes) throws IOException {
-
-        if (JavaUtil.isGreaterOrEqual(17, 0)) {
-            copyJava7(source, destination, copyAttributes);
-        } else {
-            // Java 6
-            FileInputStream fis = null;
-            FileOutputStream fos = null;
-            try {
-            	fis = new FileInputStream(source);
-            	fos =  new FileOutputStream(destination);
-                FileChannel sourceChannel = fis.getChannel();
-                FileChannel destChannel = fos.getChannel();
-                long size = sourceChannel.size();
-                long transferedBytes = 0;
-                while (transferedBytes < size) {
-                    transferedBytes += destChannel.transferFrom(sourceChannel, 0, size - transferedBytes);
-                }
-                destination.setWritable(source.canWrite());
-                // obviously, source is readable
-                destination.setExecutable(source.canExecute());
-                if (!destination.setLastModified(source.lastModified())) {
-                	throw new IOException("Couldn't update last-modified of '" + destination
-                			+ "' to " + source.lastModified());
-                }
-            } finally {
-                close(fis, fos);
-            }
-        }
-    }
-
-    @IgnoreJRERequirement
-    private static void copyJava7(File source, File destination, boolean copyAttributes) throws IOException {
         Path sourcePath = source.toPath();
         Path targetPath = destination.toPath();
         if (copyAttributes) {
@@ -79,6 +41,10 @@ public class IOUtils {
         }
     }
 
+    /**
+     * @deprecated please use Java 7 try-with-resources pattern!
+     */
+    @Deprecated
     public static void close(Closeable closeable) {
         if (closeable != null) {
             try {
@@ -89,6 +55,10 @@ public class IOUtils {
         }
     }
     
+    /**
+     * @deprecated please use Java 7 try-with-resources pattern!
+     */
+    @Deprecated
     public static void close(Closeable... closables) {
         for (Closeable closeable : closables) {
         	close(closeable);
