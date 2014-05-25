4b8b9289631d1818a2f17834f634dcdfb6c686a5
Christoph Kutzinski <kutzi@gmx.de>
2014-05-25 11:56:42 +0200

    Use try-with-resources

--- wremja-core/src/main/java/com/kemai/wremja/gui/actions/AbstractExportAction.java
+++ wremja-core/src/main/java/com/kemai/wremja/gui/actions/AbstractExportAction.java
@@ -19,8 +19,6 @@ import com.kemai.wremja.gui.settings.IUserSettings;
 import com.kemai.wremja.logging.Logger;
 import com.kemai.wremja.model.export.Exporter;
 
-import de.kutzi.javautils.io.IOUtils;
-
 /**
  * Base action for all data exports.
  * @author remast
@@ -28,10 +26,6 @@ import de.kutzi.javautils.io.IOUtils;
 @SuppressWarnings("serial")
 public abstract class AbstractExportAction extends AbstractWremjaAction {
 
-    /** The bundle for internationalized texts. */
-    private static final TextResourceBundle textBundle = TextResourceBundle.getBundle(AbstractExportAction.class);
-
-    /** The logger. */
     private static final Logger log = Logger.getLogger(AbstractExportAction.class);
 
     private final IUserSettings settings;
@@ -110,15 +104,16 @@ public abstract class AbstractExportAction extends AbstractWremjaAction {
 
     }
     
-    private static OutputStream getFileOutputStream(File f, FileFilter fileFilter, Frame owner) {
+    private OutputStream getFileOutputStream(File f, FileFilter fileFilter, Frame owner) {
         try {
             return new BufferedOutputStream(new FileOutputStream(f));
         } catch (FileNotFoundException e) {
             log.error(e, e);
+            TextResourceBundle bundle = TextResourceBundle.getBundle(getClass());
             JOptionPane.showMessageDialog(
                     owner, 
-                    textBundle.textFor("AbstractExportAction.IOException.Message", f.getAbsolutePath(), e.getLocalizedMessage()), //$NON-NLS-1$
-                    textBundle.textFor("AbstractExportAction.IOException.Heading", fileFilter.getDescription()), //$NON-NLS-1$
+                    bundle.textFor("AbstractExportAction.IOException.Message", f.getAbsolutePath(), e.getLocalizedMessage()), //$NON-NLS-1$
+                    bundle.textFor("AbstractExportAction.IOException.Heading", fileFilter.getDescription()), //$NON-NLS-1$
                     JOptionPane.ERROR_MESSAGE
             );
             return null;
@@ -132,55 +127,48 @@ public abstract class AbstractExportAction extends AbstractWremjaAction {
     protected class ExportWorker extends SwingWorker<String, Object> {
         
         private PresentationModel model;
-        
         private File file;
-        private OutputStream out;
-        
         private Exporter exporter;
-        
         private Frame owner;
-        
         private FileFilter fileFilter;
 
         public ExportWorker(final PresentationModel model, final Exporter exporter, final FileFilter fileFilter, final Frame owner, final File file) {
-            this( model, exporter, owner, getFileOutputStream(file, fileFilter, owner));
+            this.model = model;
+            this.exporter = exporter;
+            this.owner = owner;
             this.fileFilter = fileFilter;
             this.file = file;
         }
         
-        public ExportWorker(final PresentationModel model, final Exporter exporter, final Frame owner, final OutputStream out) {
-            this.model = model;
-            this.exporter = exporter;
-            this.owner = owner;
-            this.out = out;
-        }
-
         @Override
         public String doInBackground() {
-            try {
-                synchronized ( model.getData() ) {
-                    exporter.export(
-                            model.getData(),
-                            model.getFilter(),
-                            out
-                    );
-                }
-                
-                // Make sure everything is written.
-                out.flush();
 
-                // Store export location in settings
-                setLastExportLocation(file.getAbsolutePath());
+            try {
+            	try (OutputStream out = getFileOutputStream(file, fileFilter, owner)) {
+
+	                synchronized ( model.getData() ) {
+	                    exporter.export(
+	                            model.getData(),
+	                            model.getFilter(),
+	                            out
+	                    );
+	                }
+	                
+	                // Make sure everything is written.
+	                out.flush();
+	
+	                // Store export location in settings
+	                setLastExportLocation(file.getAbsolutePath());
+        		}
             } catch (Exception e) {
                 log.error(e, e);
+                TextResourceBundle bundle = TextResourceBundle.getBundle(getClass());
                 JOptionPane.showMessageDialog(
                         owner, 
-                        textBundle.textFor("AbstractExportAction.IOException.Message", file.getAbsolutePath(), e.getLocalizedMessage()), //$NON-NLS-1$
-                        textBundle.textFor("AbstractExportAction.IOException.Heading", fileFilter.getDescription()), //$NON-NLS-1$
+                        bundle.textFor("AbstractExportAction.IOException.Message", file.getAbsolutePath(), e.getLocalizedMessage()), //$NON-NLS-1$
+                        bundle.textFor("AbstractExportAction.IOException.Heading", fileFilter.getDescription()), //$NON-NLS-1$
                         JOptionPane.ERROR_MESSAGE
                 );
-            } finally {
-                IOUtils.close(out);
             }
             return null;
         }
